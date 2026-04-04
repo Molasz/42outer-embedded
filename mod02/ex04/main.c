038ed8f6d3f5af53a770793ecbeef2bfb2e7a1a4
Molasz <martiolasz@gmail.com>
2026-04-05 01:27:34 +0200

    mod02 ex04 & add header

--- mod02/ex04/main.c
+++ mod02/ex04/main.c
@@ -5,25 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: molasz-a <molasz.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/05 01:21:31 by molasz-a          #+#    #+#             */
-/*   Updated: 2026/04/05 01:26:47 by molasz-a         ###   ########.fr       */
+/*   Created: 2026/04/05 01:21:01 by molasz-a          #+#    #+#             */
+/*   Updated: 2026/04/05 01:27:10 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 void uart_init()
 {
 	UCSR0A |= (1 << U2X0);
 	UBRR0 = (F_CPU / (8UL * BAUD)) - 1;
-	UCSR0B |= (1 << TXEN0) | (1 << RXEN0);		// Enable RX
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
 	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
-}
-
-char	uart_rx(void)
-{
-	while (!(UCSR0A & (1 << RXC0)));			// Wait until USART receive
-	return UDR0;
 }
 
 void	uart_tx(char c)
@@ -32,11 +27,25 @@ void	uart_tx(char c)
 	UDR0 = c;
 }
 
+ISR(USART_RX_vect)
+{
+	char	c;
+
+	c = UDR0;
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	else if (c >= 'A' && c <= 'Z')
+		c += 32;
+	uart_tx(c);
+}
+
 int	main()
 {
+	cli();
 	uart_init();
-	while (1)
-		uart_tx(uart_rx());
+	sei();
+
+	while (1) {}
 
 	return (0);
 }
