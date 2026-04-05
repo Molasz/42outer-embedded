df1717bcb6e210cef3ee9e1dbb50fe2c6bc2ad87
Molasz <martiolasz@gmail.com>
2026-04-05 02:43:36 +0200

    mod02 ex04

--- mod02/ex04/main.c
+++ mod02/ex04/main.c
@@ -6,20 +6,29 @@
 /*   By: molasz-a <molasz.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 01:21:01 by molasz-a          #+#    #+#             */
-/*   Updated: 2026/04/05 01:27:10 by molasz-a         ###   ########.fr       */
+/*   Updated: 2026/04/05 02:41:53 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
+
+# define USER "molasz-a"
+# define PASS "pass"
 
 void uart_init()
 {
 	UCSR0A |= (1 << U2X0);
 	UBRR0 = (F_CPU / (8UL * BAUD)) - 1;
-	UCSR0B |= (1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0);
+	UCSR0B |= (1 << TXEN0) | (1 << RXEN0);
 	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
 }
+
+char	uart_rx(void)
+{
+	while (!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
 
 void	uart_tx(char c)
 {
@@ -27,25 +36,64 @@ void	uart_tx(char c)
 	UDR0 = c;
 }
 
-ISR(USART_RX_vect)
+void	uart_printstr(const char *str)
 {
-	char	c;
+	int	i = 0;
 
-	c = UDR0;
-	if (c >= 'a' && c <= 'z')
-		c -= 32;
-	else if (c >= 'A' && c <= 'Z')
-		c += 32;
-	uart_tx(c);
+	while (str[i])
+		uart_tx(str[i++]);
+}
+
+int	validate_str(const char *str, char hide)
+{
+	char	c = 0;
+	int		r = 0;
+	int		i = 0;
+
+	while (c != '\r')
+	{
+		c = uart_rx();
+		if (c > 31 && c != 127)
+		{
+			if (!r && str[i])
+			{
+				if (str[i])
+					r |= str[i] != c;
+				else
+					r = 1;
+			}
+			i++;
+		}
+
+		if (hide && c > 31 && c != 127)
+			uart_tx(hide);
+		else
+			uart_tx(c);
+	}
+	uart_printstr("\r\n");
+	return (r);
 }
 
 int	main()
 {
-	cli();
-	uart_init();
-	sei();
+	int	r = 0;
 
-	while (1) {}
+	uart_init();
+
+	while (1)
+	{
+		uart_printstr("Enter your login:\r\nusername: ");
+		r |= validate_str(USER, 0);
+		uart_printstr("password: ");
+		r |= validate_str(PASS, '*');
+		if (r)
+			uart_printstr("Bad combination username/password\r\n");
+		else
+		{
+			uart_printstr("Welcome!\r\n");
+		}
+		uart_printstr("\r\n");
+	}
 
 	return (0);
 }
