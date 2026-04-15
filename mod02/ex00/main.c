9486d5dab73a944a61e75b64b961aaf435bfe572
Molasz <martiolasz@gmail.com>
2026-04-15 20:26:25 +0200

    update comments

--- mod02/ex00/main.c
+++ mod02/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 01:21:44 by molasz-a          #+#    #+#             */
-/*   Updated: 2026/04/15 19:27:23 by molasz-a         ###   ########.fr       */
+/*   Updated: 2026/04/15 20:25:23 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@
 void uart_init()
 {												// 19.10
 	UCSR0A |= (1 << U2X0);						// USART speed x2
-	UBRR0 = (F_CPU / (8UL * BAUD)) - 1;			// Baud speed | UBBR0H & UBBR0L 19.10.5
+	UBRR0 = (F_CPU / (8UL * BAUD)) - 1;			// Baud rate | 12bits UBBR0H & UBBR0L 19.10.5
+												// Each n cycles send data | 16M / (8 / 115200) = 16,...
 	UCSR0B |= (1 << TXEN0);						// Enable USART
 	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);	// 8 bits           8
 	UCSROC &= ~((1 << UPM00) | (1 << UPM01));	// Parity disabled  N
