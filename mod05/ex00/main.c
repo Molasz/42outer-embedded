52afcea9b973e10356c77e63cbaae78e9943986e
Molasz <martiolasz@gmail.com>
2026-04-17 21:28:09 +0200

    use auto trigger on ex00

--- mod05/ex00/main.c
+++ mod05/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz.dev@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/05 01:21:38 by molasz-a          #+#    #+#             */
-/*   Updated: 2026/04/17 15:09:58 by molasz-a         ###   ########.fr       */
+/*   Updated: 2026/04/17 21:23:09 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,16 +49,9 @@ char	*int_hex(uint8_t n)
 void timer_init()
 {
 	TCCR1B |= (1 << WGM12);
-	TIMSK1 |= (1 << OCIE1A);
 	OCR1A = 312;
+	OCR1B = 312;
 	TCCR1B |= (1 << CS12) | (1 << CS10);
-}
-
-void	TIMER1_COMPA_vect() __attribute__((signal));
-
-void	TIMER1_COMPA_vect()
-{
-	ADCSRA |= (1 << ADSC);
 }
 
 void	ADC_vect() __attribute__((signal));
@@ -67,6 +60,7 @@ void	ADC_vect()
 {
 	uart_printstr(int_hex(ADCH));
 	uart_printstr("\r\n");
+	TIFR1 |= (1 << OCF1B);												// Clear timer flag
 }
 
 int	main()
@@ -74,8 +68,10 @@ int	main()
 	ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));	// Input channel ADC0 (23-4)
 	ADMUX |= (1 << REFS0) | (1 << ADLAR);								// 0 - 1 AVCC max reference (23-3) | Align left
 
-	ADCSRA |= (1 << ADEN) | (1 << ADIE);								// Enable ADC & ADC interrupt
+	ADCSRA |= (1 << ADEN) | (1 << ADIE) | (1 << ADATE);					// Enable ADC | ADC interrupt | Enable Auto trigger (23-6)
 	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);				// 128 prescaler
+
+	ADCSRB |= (1 << ADTS2) | (1 << ADTS0);								// Trigger ADC conversion on timer match
 
 	uart_init();
 	timer_init();
